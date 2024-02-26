fa9bd3fff75388856d524394fa5a9de7fff0a6db
Luiz Paulo <luizpaulo@MacBook-Air-de-Luiz.local>
2024-02-26 05:33:38 -0300

    first commit

---
+++ src/test/java/hibridus_rest/hibridus_rest/AppTest.java
@@ -0,0 +1,20 @@
+package hibridus_rest.hibridus_rest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
